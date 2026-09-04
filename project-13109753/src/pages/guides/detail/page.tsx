5e5e5173f99bd5bb8a2719964450d9e5629222b1
massaozza <m.ozawa@shonanhuaro.jp>
2026-09-04 13:42:55 +0900

    Import LocalizedLink component in page.tsx

--- project-13109753/src/pages/guides/detail/page.tsx
+++ project-13109753/src/pages/guides/detail/page.tsx
@@ -1,3 +1,4 @@
+import LocalizedLink from '@/components/feature/LocalizedLink';
 import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import Navbar from '@/components/feature/Navbar';
