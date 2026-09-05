f275fbe4a1a94c0a126be5241d393e8c1b6ee5ff
massaozza <m.ozawa@shonanhuaro.jp>
2026-09-06 08:26:11 +0900

    Add 'nav_profile' translation in Indonesian

--- project-13109753/src/i18n/local/id/common.ts
+++ project-13109753/src/i18n/local/id/common.ts
@@ -30,6 +30,7 @@ const translations = {
   hero_cta_plan: 'สร้างทริปญี่ปุ่นที่ออกแบบมาเพื่อคุณ',
   hero_cta_explore: 'สำรวจญี่ปุ่น',
   hero_subtitle_explore: 'ดูทริปจริงและข้อมูลท้องถิ่น',
+  nav_profile: 'Profil',
 };
 
 export default translations;
