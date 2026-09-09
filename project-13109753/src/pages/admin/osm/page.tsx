ea5d43a4fa8e764788a0e9e70e74ceff8bb235a1
massaozza <m.ozawa@shonanhuaro.jp>
2026-09-09 20:17:18 +0900

    Add files via upload

--- project-13109753/src/pages/admin/osm/page.tsx
+++ project-13109753/src/pages/admin/osm/page.tsx
@@ -224,7 +224,7 @@ export default function AdminOsmPage() {
     }
   };
 
-
+  const input =
     'bg-white border border-background-200 rounded-md px-3 py-2 text-sm text-foreground-900 focus:outline-none focus:ring-2 focus:ring-primary-400';
 
   return (
